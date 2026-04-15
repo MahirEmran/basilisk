93a9a501b619c185a502abfc7962fef93e27d0a7
Leah Kiner <leahannkiner@gmail.com>
2026-04-15 17:05:51 -0600

    [#1359] Add new spacecraft charging dynamics module

---
+++ src/simulation/dynamics/spacecraftCharging/spacecraftCharging.h
@@ -0,0 +1,99 @@
+/*
+ ISC License
+
+ Copyright (c) 2025, Autonomous Vehicle Systems Lab, University of Colorado at Boulder
+
+ Permission to use, copy, modify, and/or distribute this software for any
+ purpose with or without fee is hereby granted, provided that the above
+ copyright notice and this permission notice appear in all copies.
+
+ THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+
+ */
+
+#ifndef SPACECRAFT_CHARGING_H
+#define SPACECRAFT_CHARGING_H
+
+#include "simulation/dynamics/_GeneralModuleFiles/stateData.h"
+#include "simulation/dynamics/_GeneralModuleFiles/dynParamManager.h"
+#include "simulation/dynamics/_GeneralModuleFiles/dynamicObject.h"
+#include "simulation/dynamics/_GeneralModuleFiles/stateVecIntegrator.h"
+#include "architecture/_GeneralModuleFiles/sys_model.h"
+#include "architecture/messaging/messaging.h"
+#include "architecture/msgPayloadDefC/CurrentMsgPayload.h"
+#include "architecture/msgPayloadDefC/ElectronBeamMsgPayload.h"
+#include "architecture/msgPayloadDefC/ProjectedAreaMsgPayload.h"
+#include "architecture/msgPayloadDefC/VoltMsgPayload.h"
+#include "architecture/utilities/bskLogging.h"
+
+/*! @brief spacecraft charging module */
+class SpacecraftCharging : public DynamicObject{
+public:
+    SpacecraftCharging();
+    ~SpacecraftCharging() = default;
+    void initializeDynamics();
+    void Reset(uint64_t CurrentSimNanos);
+    void registerStates(DynParamManager& states);
+    void writeOutputStateMessages(uint64_t clockTime);
+    void UpdateState(uint64_t CurrentSimNanos);
+    void equationsOfMotion(double integTimeSeconds, double timeStep);
+    void computeElectronBeamCurrent();
+    void computePhotoelectricCurrent();
+    void preIntegration(uint64_t callTimeNanos) final;
+    void postIntegration(uint64_t callTimeNanos) final;
+
+    void setEBeamCurrent(const double I_eBeam);  //!< Setter for the electron beam current
+    void setEBeamEnergy(const double E_eBeam);  //!< Setter for the electron beam energy
+    void setServicerCapacitance(const double capacitance);  //!< Setter for the servicer capacitance
+    void setTargetCapacitance(const double capacitance);  //!< Setter for the target capacitance
+    double getEBeamCurrent() const;  //!< Getter for the electron beam current
+    double getEBeamEnergy() const;  //!< Getter for the electron beam energy
+    double getServicerCapacitance() const;  //!< Getter for the servicer capacitance
+    double getTargetCapacitance() const;  //!< Getter for the target capacitance
+
+    ReadFunctor<ElectronBeamMsgPayload> electronBeamInMsg; //!< Electron beam input message
+    ReadFunctor<ProjectedAreaMsgPayload> servicerSunlitAreaInMsg;  //!< Total servicer sunlit facet area input message
+    ReadFunctor<ProjectedAreaMsgPayload> targetSunlitAreaInMsg;  //!< Total target sunlit facet area input message
+
+    Message<VoltMsgPayload> servicerPotentialOutMsg;  //!< Servicer potential (voltage) output message
+    Message<VoltMsgPayload> targetPotentialOutMsg;  //!< Target potential (voltage) output message
+    Message<CurrentMsgPayload> servicerEBCurrentOutMsg;  //!< Servicer electron beam current output message
+    Message<CurrentMsgPayload> targetEBCurrentOutMsg;  //!< Target electron beam current output message
+    Message<CurrentMsgPayload> servicerPECurrentOutMsg;  //!< Servicer photoelectric current output message
+    Message<CurrentMsgPayload> targetPECurrentOutMsg;  //!< Target photoelectric current output message
+
+    BSKLogger bskLogger;  //!< BSK Logging
+
+private:
+    double electronBeamEnergy{};  //!< [keV] Electron beam energy
+    double electronBeamCurrent{};  //!< [Amps] Electron beam current
+    double alphaEB{};  //!< [-] Scaling term for the fraction of current reaching the target
+
+    double servicerCapacitance{};  //!< [farads] Servicer capacitance
+    double targetCapacitance{};  //!< [farads] Target capacitance
+    double servicerPotentialInit{};  //!< [Volts] Initial servicer potential
+    double targetPotentialInit{};  //!< [Volts] Initial target potential
+    double servicerSunlitArea{};  //!< [m^2] Servicer sunlit area
+    double targetSunlitArea{};  //!< [m^2] Target sunlit area
+
+    double servicerPotential{};  //!< [Volts] Servicer potential
+    double targetPotential{};  //!< [Volts] Target potential
+    double servicerEBCurrent{};  //!< [Amps] Servicer electron beam current
+    double targetEBCurrent{};  //!< [Amps] Target electron beam current
+    double servicerPECurrent{};  //!< [Amps] Servicer photoelectric current
+    double targetPECurrent{};  //!< [Amps] Target photoelectric current
+
+    StateData *servicerPotentialState = nullptr;  //!< State data container for servicer potential
+    StateData *targetPotentialState = nullptr;  //!< State data container for target potential
+    std::string nameOfServicerPotentialState;
+    std::string nameOfTargetPotentialState;
+};
+
+
+#endif /* SPACECRAFT_CHARGING_H */
